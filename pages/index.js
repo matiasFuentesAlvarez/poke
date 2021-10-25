1709739a94e5a3da1562d42a85292050fc5a8c27
matiasFuentesAlvarez <matias.fuentes@alumnos.uach.cl>
2021-10-25 15:54:06 -0300

    nuevo cambio

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Pokemon = ({ pokemon }) => {
 export default function Pokemones({ pokemones }) {
   return (
     <div>
-      <p>Pokemones</p>
+      <p>Mi app de Pokemones</p>
       <ul>
         {pokemones.map(pokemon => <Pokemon pokemon={pokemon} key={pokemon.name} />)}
       </ul>
